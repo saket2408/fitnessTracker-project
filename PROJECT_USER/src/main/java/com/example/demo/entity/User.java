c544c38170efe55dcf63493f5fe4beccbacb6cde
b4ibmjava08@iiht.tech <b4ibmjava08@iiht.tech>
2019-09-19 23:56:43 +0530

    added microservices

--- PROJECT_USER/src/main/java/com/example/demo/entity/User.java
+++ PROJECT_USER/src/main/java/com/example/demo/entity/User.java
@@ -7,9 +7,9 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="user")
+@Table(name = "user")
 public class User {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
@@ -23,15 +23,10 @@ public class User {
 	private String gender;
 	private double bmi;
 	private String category;
-	
+
 	public User() {
 		super();
 	}
-
-
-	
-
-	
 
 	public User(int id, String firstName, String lastName, String email, String password, double height, double weight,
 			int age, String gender) {
@@ -46,14 +41,6 @@ public class User {
 		this.age = age;
 		this.gender = gender;
 	}
-
-
-	
-
-
-
-
-	
 
 	public User(int id, String firstName, String lastName, String email, String password, double height, double weight,
 			int age, String gender, String category, double bmi) {
@@ -71,142 +58,92 @@ public class User {
 		this.bmi = bmi;
 	}
 
-
-
-
-
-
 	public int getId() {
 		return id;
 	}
-
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
 	public String getFirstName() {
 		return firstName;
 	}
-
 
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
-
 	public String getLastName() {
 		return lastName;
 	}
-
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	public String getPassword() {
 		return password;
 	}
-
 
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-
 	public double getHeight() {
 		return height;
 	}
-
 
 	public void setHeight(double height) {
 		this.height = height;
 	}
 
-
 	public double getWeight() {
 		return weight;
 	}
-
 
 	public void setWeight(double weight) {
 		this.weight = weight;
 	}
 
-
 	public int getAge() {
 		return age;
 	}
-
 
 	public void setAge(int age) {
 		this.age = age;
 	}
 
-
 	public String getGender() {
 		return gender;
 	}
-
 
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
 
-
 	public String getCategory() {
 		return category;
 	}
-
-
-	
-	
-
 
 	public void setCategory(String category) {
 		this.category = category;
 	}
 
-
-
-
-
-
 	public double getBmi() {
 		return bmi;
 	}
 
-
-
-
-
-
 	public void setBmi(double bmi) {
 		this.bmi = bmi;
 	}
-
-
-	
-
-
-
-
-
-	
-	
-	
-	
 
 }
